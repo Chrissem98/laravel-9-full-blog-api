f2aa59e2c5d34048659e551b6bc24f2e7918a32b
Chris <chrissem19@gmail.com>
2022-04-14 08:46:17 +0000

    Database: create basique post table migration, model, controller...

---
+++ database/migrations/2022_04_14_075806_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description')->nullable();
+            $table->timestamps();
+
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+            $table->foreignId('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
